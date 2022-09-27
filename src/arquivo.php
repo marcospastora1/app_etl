b98e7f3852e4a473e3706ef7e768081b5a1222fa
Marcos <marcos.santos@codenapp.com>
2022-09-27 18:51:06 -0300

    aula 18

---
+++ src/arquivo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace src;
+
+class Arquivo
+{
+  private $dados = array();
+
+  public function setDados(string $nome, string $cpf, string $email): void
+  {
+    array_push(
+      $this->dados,
+      [
+        'nome' => iconv('iso-8859-1', 'utf-8', $nome),
+        'cpf' => $cpf,
+        'email' => $email
+      ]
+    );
+  }
+
+  public function getDados(): array
+  {
+    return $this->dados;
+  }
+
+  public function lerArquivoCSV($caminho): void
+  {
+    $handle = fopen($caminho, 'r');
+
+    while (($linha = fgetcsv($handle, 10000, ';')) !== false) {
+      $this->setDados($linha[0], $linha[1], $linha[2]);
+    }
+  }
+}
